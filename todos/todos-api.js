355af69817c5e0eba89dc88f281c83f821e91a1f
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2018-04-22 17:30:06 -0700

    add DELETE /todos/:id API and tests

--- todos/todos-api.js
+++ todos/todos-api.js
@@ -39,4 +39,22 @@ module.exports = function todoApis (app) {
       })
       .catch(e => res.status(400).send());
   });
+
+  app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).send();
+        }
+
+        return res.send({ todo });
+      })
+      .catch(e => res.status(400).send());
+  });
 };
