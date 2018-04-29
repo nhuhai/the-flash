06053e33f560ce8d6b36017df76a8f9ebefa627e
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2018-04-29 00:29:35 -0700

    add more dependencies

--- server.js
+++ server.js
@@ -3,25 +3,46 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const cors = require('cors');
+const passport = require('passport');
+const errorhandler = require('errorhandler');
+
 const logAndSaveRequest = require('./middlewares/log-and-save-request');
-const serveViews = require('./serve-views');
-const itemsApi = require('./api/items');
+// const serveViews = require('./serve-views');
+// const itemsApi = require('./api/items');
 
 const { mongoose } = require('./db/mongoose');
-const { User } = require('./models/user');
-const { ObjectID } = require('mongodb');
 
 const port = process.env.PORT;
+const isProduction = process.env.NODE_ENV === 'production';
+
 const app = express();
-
-app.use(logAndSaveRequest);
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
 app.use(cors());
+app.use(require('morgan')('dev'));
+// app.use(logAndSaveRequest);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-serveViews(app);
-itemsApi(app);
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+  secret: 'green-arrow',
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
+}));
+
+if (!isProduction) {
+  app.use(errorhandler());
+  mongoose.set('debug', true);
+}
+
+app.use(require('./routes'));
+
+// serveViews(app);
+// itemsApi(app);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is up on port ${port}`)
